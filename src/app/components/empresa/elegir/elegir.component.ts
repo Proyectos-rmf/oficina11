f089cafbc51b85e1acda6904ee8e1b957f8d9361
Proyectos-rmf <56318058+Proyectos-rmf@users.noreply.github.com>
2021-01-27 14:54:32 -0600

    elegir
    elegir

--- src/app/components/empresa/elegir/elegir.component.ts
+++ src/app/components/empresa/elegir/elegir.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UtilService } from '../../../services/util.service';
 
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
 import { Empresa } from 'src/app/models/empresa';
+import { EmpresaService } from '../empresas.service';
 
 @Component({
   selector: 'app-elegir',
@@ -9,16 +13,34 @@ import { Empresa } from 'src/app/models/empresa';
   styleUrls: ['./elegir.component.scss']
 })
 export class ElegirComponent implements OnInit {
-  empresas$ = this.UTIL.ApuntadorAction$;
-  Empresamodal: Empresa[];
+  // empresas$ = this.UTIL.ApuntadorAction$;
+  empresas$ = this.empresaSvc.empresas;
+  // Empresamodal: Empresa[];
+  // dataSource: Empresa[];
 
-  constructor(private UTIL: UtilService) {}
+  ELEMENT_DATA: Empresa[];
+
+  displayedColumns: string[] = ['Nombre','Domicilio'];
+  dataSource: any;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private UTIL: UtilService, private empresaSvc: EmpresaService) {}
 
   ngOnInit(): void {
-    this.empresas$.subscribe(res => { this.Empresamodal = res });
+    this.empresas$.subscribe(res => {  this.ELEMENT_DATA = res });
 
     setTimeout(() => {
+      // this.ELEMENT_DATA = this.Empresamodal;
+      this.dataSource = new MatTableDataSource<Empresa>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+      console.log(this.ELEMENT_DATA);
+      console.log(this.dataSource.paginator);
+
+
+      // this.dataSource = this.Empresamodal;
+
       // console.log(this.Empresamodal[0].nombre_Emp);
-    }, 1000);
+    }, 2000);
   }
 }
