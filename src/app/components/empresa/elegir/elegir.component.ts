019c7d32ca5284fbee9731256187acd660d68f7c
Proyectos-rmf <56318058+Proyectos-rmf@users.noreply.github.com>
2021-01-29 09:52:16 -0600

    varios
    varios

--- src/app/components/empresa/elegir/elegir.component.ts
+++ src/app/components/empresa/elegir/elegir.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { UtilService } from '../../../services/util.service';
 
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
@@ -13,11 +12,7 @@ import { EmpresaService } from '../empresas.service';
   styleUrls: ['./elegir.component.scss']
 })
 export class ElegirComponent implements OnInit {
-  // empresas$ = this.UTIL.ApuntadorAction$;
   empresas$ = this.empresaSvc.empresas;
-  // Empresamodal: Empresa[];
-  // dataSource: Empresa[];
-
   ELEMENT_DATA: Empresa[];
 
   displayedColumns: string[] = ['Nombre','Domicilio'];
@@ -25,22 +20,17 @@ export class ElegirComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private UTIL: UtilService, private empresaSvc: EmpresaService) {}
+  constructor(private empresaSvc: EmpresaService) {}
 
   ngOnInit(): void {
-    this.empresas$.subscribe(res => {  this.ELEMENT_DATA = res });
+    this.empresas$.subscribe(res => { this.ELEMENT_DATA = res });
 
     setTimeout(() => {
-      // this.ELEMENT_DATA = this.Empresamodal;
       this.dataSource = new MatTableDataSource<Empresa>(this.ELEMENT_DATA);
       this.dataSource.paginator = this.paginator;
+
       console.log(this.ELEMENT_DATA);
       console.log(this.dataSource.paginator);
-
-
-      // this.dataSource = this.Empresamodal;
-
-      // console.log(this.Empresamodal[0].nombre_Emp);
     }, 2000);
   }
 }
